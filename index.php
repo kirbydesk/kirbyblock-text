614c0c17e3866acface8ff5be10534644b75f758
Christian Fickinger <chrfickinger@users.noreply.github.com>
2025-10-16 10:46:26 +0200

    Plugin structure

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php Kirby::plugin('chrfickinger/kirbyblock-text', [
+
+	/* -------------- Extensions --------------*/
+	'translations' => require_once 'src/extensions/translations.php',
+
+]);
---
+++ src/extensions/translations.php
@@ -0,0 +1,6 @@
+<?php return [
+
+	/* -------------- Translations --------------*/
+	'de' => require_once __DIR__ . '/../i18n/de.php',
+	'en' => require_once __DIR__ . '/../i18n/en.php'
+];
