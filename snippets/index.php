345e5ef3627be99fde0729fa9bda6f531d6ccbb8
Christian Fickinger <mail@christianfickinger.de>
2026-02-06 17:50:52 +0100

    WIP

--- snippets/index.php
+++ snippets/index.php
@@ -4,14 +4,6 @@
 echo '<section';
 echo ' data-block="text"';
 echo ' data-block-id="b'.$block->id().'"';
-echo ' data-grid-size-sm="'.$block->gridsizesm()->value().'"';
-echo ' data-grid-size-md="'.$block->gridsizemd()->value().'"';
-echo ' data-grid-size-lg="'.$block->gridsizelg()->value().'"';
-echo ' data-grid-size-xl="'.$block->gridsizexl()->value().'"';
-echo ' data-grid-offset-sm="'.$block->gridoffsetsm()->value().'"';
-echo ' data-grid-offset-md="'.$block->gridoffsetmd()->value().'"';
-echo ' data-grid-offset-lg="'.$block->gridoffsetlg()->value().'"';
-echo ' data-grid-offset-xl="'.$block->gridoffsetxl()->value().'"';
 echo ' data-margin-top="'.$block->margintop()->value().'"';
 echo ' data-margin-bottom="'.$block->marginbottom()->value().'"';
 echo ' data-padding-top="'.$block->paddingtop()->value().'"';
@@ -20,21 +12,30 @@ echo ' data-style="'.$block->style()->value().'"';
 echo $block->fragment()->isNotEmpty() ? ' id="'.$block->fragment()->value().'"' : '';
 echo '>'."\n";
 
+// Grid
+echo '<div data-layout="grid"><div';
+echo ' data-grid-size-sm="'.$block->gridsizesm()->value().'"';
+echo ' data-grid-size-md="'.$block->gridsizemd()->value().'"';
+echo ' data-grid-size-lg="'.$block->gridsizelg()->value().'"';
+echo ' data-grid-size-xl="'.$block->gridsizexl()->value().'"';
+echo ' data-grid-offset-sm="'.$block->gridoffsetsm()->value().'"';
+echo ' data-grid-offset-md="'.$block->gridoffsetmd()->value().'"';
+echo ' data-grid-offset-lg="'.$block->gridoffsetlg()->value().'"';
+echo ' data-grid-offset-xl="'.$block->gridoffsetxl()->value().'"';
+echo '>'."\n";
 
 // Tagline
 snippet('field/tagline', ['content' => $block]);
 
-
 // Heading
 snippet('field/heading', ['content' => $block]);
 
-
 // Text
 snippet('field/text', ['content' => $block]);
-
 
 // Buttons
 snippet('field/buttons', ['content' => $block]);
 
 
+echo '</div></div>'."\n"; // End Grid
 echo '</section>'."\n";
